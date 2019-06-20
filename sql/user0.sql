7511f48283cfc2065e18eca19ea8c9e57a73c2ff
qudaiyou <597935388@qq.com>
2019-06-20 19:44:28 +0800

    add database sql

---
+++ sql/user0.sql
@@ -0,0 +1,40 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50721
+Source Host           : 127.0.0.1:3306
+Source Database       : user0
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-06-20 19:42:25
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user_info0
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info0`;
+CREATE TABLE `user_info0` (
+  `user_id` bigint(19) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `account` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_info1
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info1`;
+CREATE TABLE `user_info1` (
+  `user_id` bigint(19) NOT NULL,
+  `user_name` varchar(45) DEFAULT NULL,
+  `account` varchar(45) NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
